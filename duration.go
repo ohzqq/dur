035eec6023a27afa4b5d18034d58e9ec2f380e64
ohzqq <iamchurkey@gmail.com>
2023-03-20 14:02:52 -0400

    Dur

--- duration.go
+++ duration.go
@@ -1,70 +1,102 @@
 package dur
 
 import (
+	"fmt"
 	"log"
+	"strconv"
 	"time"
 )
 
 type Duration time.Duration
 
-type DurFmt string
+type DurFmt int
 
+//go:generate stringer -type=DurFmt
 const (
-	HH         DurFmt = "15"
-	MM                = "04"
-	SS                = "05"
-	MMSS              = MM + ":" + SS
-	HHMMSS            = HH + ":" + MMSS
-	HHMMSSsss         = HHMMSS + ".000"
-	Timestamp         = HHMMSS
-	Cuestamp          = MMSS
-	MilliStamp        = HHMMSSsss
+	HH DurFmt = iota
+	MM
+	SS
+	MMSS
+	HHMMSS
+	HHMMSSsss
+	Timestamp
+	Cuestamp
+	MilliStamp
 )
 
-func Parse(format, dur string) Duration {
-	switch DurFmt(format) {
+const (
+	nano   = 1000000000
+	ssNano = nano
+	mmNano = 60 * nano
+	hhNano = 60 * 60 * nano
+)
+
+func Parse(format DurFmt, dur string) Duration {
+	ds := "0s"
+	switch format {
 	case HH:
-		d, err := time.ParseDuration(dur + "h")
-		if err != nil {
-			log.Fatal(err)
-		}
-		return Duration(d)
+		h, _ := strconv.Atoi(dur)
+		hh := int64(h * hhNano)
+		return Duration(time.Duration(hh))
 	case MM:
-		d, err := time.ParseDuration(dur + "m")
-		if err != nil {
-			log.Fatal(err)
-		}
-		return Duration(d)
+		m, _ := strconv.Atoi(dur)
+		mm := int64(m * mmNano)
+		return Duration(time.Duration(mm))
 	case SS:
-		//ss, err := strconv.Atoi(dur)
-		//if err != nil {
-		//  log.Fatal(err)
-		//}
-		d, err := time.ParseDuration(dur + "s")
+		s, _ := strconv.Atoi(dur)
+		ss := int64(s * ssNano)
+		return Duration(time.Duration(ss))
+	case MMSS:
+		var mm, ss int64
+		_, err := fmt.Sscanf(dur, MMSS, &mm, &ss)
 		if err != nil {
 			log.Fatal(err)
 		}
-		return Duration(d)
-	case MMSS:
+		s := ss * ssNano
+		m := mm * mmNano
+		return Duration(time.Duration(m + s))
 	case HHMMSS:
+		var hh, mm, ss int64
+		_, err := fmt.Sscanf(dur, HHMMSS, &hh, &mm, &ss)
+		if err != nil {
+			log.Fatal(err)
+		}
+		s := ss * ssNano
+		m := mm * mmNano
+		h := hh * hhNano
+		return Duration(time.Duration(h + m + s))
 	case HHMMSSsss:
+		var hh, mm int64
+		var ss float64
+		_, err := fmt.Sscanf(dur, HHMMSSsss, &hh, &mm, &ss)
+		if err != nil {
+			log.Fatal(err)
+		}
+		println(0.033 * ssNano)
+		s := int64(ss * ssNano)
+		m := mm * mmNano
+		h := hh * hhNano
+		return Duration(time.Duration(h + m + s))
 	}
-	def, _ := time.ParseDuration("0s")
+	def, err := time.ParseDuration(ds)
+	if err != nil {
+		log.Fatal(err)
+	}
 	return Duration(def)
 }
 
-func formatHH(hh string) string {
-	//hh, err := strconv.Atoi(hh)
-	//if err != nil {
-	//  log.Fatal(err)
-	//}
-	return hh + "h"
+func formatHH(hh int) string {
+	return strconv.Itoa(hh) + "h"
 }
 
-func formatMM(dur string) string {
-	//mm, err := strconv.Atoi(dur)
-	//if err != nil {
-	//  log.Fatal(err)
-	//}
-	return dur + "m"
+func formatMM(dur int) string {
+	return strconv.Itoa(dur) + "m"
+}
+
+func formatSS(dur int) string {
+	return strconv.Itoa(dur) + "s"
+}
+
+func formatSSsss(dur float64) string {
+	return strconv.FormatFloat(dur, 'f', 3, 64) + "s"
 }
